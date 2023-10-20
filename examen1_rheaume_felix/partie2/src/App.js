eb53405da2abfe9f82b8f3acb7f43ca7f9bb14c3
Felix-Rheaume <1934266@cegeplimoilou.ca>
2023-10-20 12:24:43 -0400

    Correction suite à la suppression de App.js

---
+++ examen1_rheaume_felix/partie2/src/App.js
@@ -0,0 +1,12 @@
+"https://examen1-rheaume-felix-hr6mkl3i0-felix-rheaume.vercel.app/"
+
+import './App.css';
+import Parent from './components/Parent';
+
+function App() {
+  return (
+    <Parent/>
+  );
+}
+
+export default App;
